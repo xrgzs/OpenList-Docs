18435520569a34b9a7ce4ae02118a7db3c947508
xrgzs <xrgzs@users.noreply.github.com>
2025-07-03 13:53:58 +0000

    deploy: e18e4d0b192624a838950eef8c4f08d5ffd1e3d9

---
+++ assets/paas.B-qCHLVR.js
@@ -0,0 +1,56 @@
+import{_ as c}from"./ValaxyMain.BB0f5Bi6.js";import{d as g,u as d,a as h}from"./chunks/vue-router.Cj-5T0BL.js";import{B as f,D as n,v as N,C as b,E as e,H as C,G as v,y as $}from"./framework.CEYCCZCa.js";import"./app.DEg5Tv4-.js";import"./chunks/dayjs.BX5x0S-s.js";import"./chunks/vue-i18n.QWawR7Lt.js";import"./chunks/pinia.YOQf0O82.js";import"./chunks/nprogress.BoH_DEjm.js";import"./index.gU-sKceF.js";const y=g("/guide/installation/paas",async a=>JSON.parse('{"title":"PaaS","description":"","frontmatter":{"title":"PaaS","title_zh-CN":"PaaS","icon":"iconfont icon-module","top":30,"categories":["guide","installation"]},"headers":[],"relativePath":"pages/guide/installation/paas.md","lastUpdated":1751447770000}'),{lazy:(a,o)=>a.name===o.name}),M={__name:"paas",setup(a,{expose:o}){var r;const{data:i}=y(),m=h(),p=d(),s=Object.assign(p.meta.frontmatter||{},((r=i.value)==null?void 0:r.frontmatter)||{});return m.currentRoute.value.data=i.value,$("valaxy:frontmatter",s),globalThis.$frontmatter=s,o({frontmatter:{title:"PaaS","title_zh-CN":"PaaS",icon:"iconfont icon-module",top:30,categories:["guide","installation"]}}),(t,l)=>{const u=c;return b(),f(u,{frontmatter:N(s)},{"main-content-md":n(()=>[l[0]||(l[0]=C("p",null,"N/A",-1)),v(`
+::: en
+N/A
+For specific usage, please refer to the \`README.md\` in the corresponding repository.
+
+:::
+::: zh-CN
+N/A
+具体用法请参考对应仓库中的\`README.md\`。
+:::
+
+## Claw Cloud Run { lang="en" }
+## Claw Cloud Run { lang="zh-CN" }
+::: en
+[https://console.run.claw.cloud/signin](https://console.run.claw.cloud/signin?link=UTMO60WWUZKY)
+:::
+::: zh-CN
+[https://console.run.claw.cloud/signin](https://console.run.claw.cloud/signin?link=UTMO60WWUZKY)
+:::
+
+## **Koyeb** { lang="en" }
+## **Koyeb** { lang="zh-CN" }
+::: en
+https://github.com/alist-org/alist-koyeb
+:::
+::: zh-CN
+https://github.com/alist-org/alist-koyeb
+:::
+
+## **Render** { lang="en" }
+## **Render** { lang="zh-CN" }
+::: en
+https://github.com/alist-org/alist-render
+:::
+::: zh-CN
+https://github.com/alist-org/alist-render
+:::
+
+### **Heroku** { lang="en" }
+## **Heroku** { lang="zh-CN" }
+::: en
+https://github.com/alist-org/alist-heroku-postgres
+:::
+::: zh-CN
+https://github.com/alist-org/alist-heroku-postgres
+:::
+
+### **Sealos** { lang="en" }
+## **Sealos** { lang="zh-CN" }
+::: en
+[![](https://raw.githubusercontent.com/labring-actions/templates/main/Deploy-on-Sealos.svg)](https://cloud.sealos.io/?openapp=system-template%3FtemplateName%3Dalist)
+:::
+::: zh-CN
+[![](https://raw.githubusercontent.com/labring-actions/templates/main/Deploy-on-Sealos.svg)](https://cloud.sealos.io/?openapp=system-template%3FtemplateName%3Dalist)
+:::
+`)]),"main-header":n(()=>[e(t.$slots,"main-header")]),"main-header-after":n(()=>[e(t.$slots,"main-header-after")]),"main-nav":n(()=>[e(t.$slots,"main-nav")]),"main-content-before":n(()=>[e(t.$slots,"main-content-before")]),"main-content":n(()=>[e(t.$slots,"main-content")]),"main-content-after":n(()=>[e(t.$slots,"main-content-after")]),"main-nav-before":n(()=>[e(t.$slots,"main-nav-before")]),"main-nav-after":n(()=>[e(t.$slots,"main-nav-after")]),comment:n(()=>[e(t.$slots,"comment")]),footer:n(()=>[e(t.$slots,"footer")]),aside:n(()=>[e(t.$slots,"aside")]),"aside-custom":n(()=>[e(t.$slots,"aside-custom")]),default:n(()=>[e(t.$slots,"default")]),_:3},8,["frontmatter"])}}};export{M as default,y as usePageData};
